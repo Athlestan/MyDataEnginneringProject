75d63b4bff08ba6dfde5b609daf1582533ec4654
Athlestan <fgrobeln@gmail.com>
2020-01-13 18:31:36 +0100

    Create 1_test_Extract_from_file.py

---
+++ 1_test_Extract_from_file.py
@@ -0,0 +1,11 @@
+from kafka import KafkaProducer
+from time import sleep
+from json import dumps
+from datetime import datetime
+
+producer = KafkaProducer(bootstrap_servers=['quickstart.cloudera:9092'], value_serializer=lambda x: dumps(x).encode('UTF-8'))
+
+for e in range(1,10):
+    data = {'id': datetime.now(), 'value': e}
+    producer.send('extract_data', value=data)
+    sleep(5)

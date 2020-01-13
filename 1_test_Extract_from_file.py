9fdd1c9caf7c0a821bfa29f1cc9a26416dd7fcca
Athlestan <fgrobeln@gmail.com>
2020-01-13 18:40:32 +0100

    Update 1_test_Extract_from_file.py

--- 1_test_Extract_from_file.py
+++ 1_test_Extract_from_file.py
@@ -6,6 +6,6 @@ from datetime import datetime
 producer = KafkaProducer(bootstrap_servers=['quickstart.cloudera:9092'], value_serializer=lambda x: dumps(x).encode('UTF-8'))
 
 for e in range(1,10):
-    data = {'id': now.strftime("%Y-%m-%d:%H:%M:%S"), 'value': e}
+    data = {'id': datetime.now().strftime("%Y-%m-%d:%H:%M:%S"), 'value': e}
     producer.send('extract_data', value=data)
     sleep(5)
